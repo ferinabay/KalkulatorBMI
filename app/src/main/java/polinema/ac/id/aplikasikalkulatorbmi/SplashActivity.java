08a0b5b43293bd0a63aca50db319651caf24f669
ferinabay <ferinabayu@gmail.com>
2019-07-11 20:16:41 +0700

    upload

---
+++ app/src/main/java/polinema/ac/id/aplikasikalkulatorbmi/SplashActivity.java
@@ -0,0 +1,26 @@
+package polinema.ac.id.aplikasikalkulatorbmi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashbmi);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },1000);
+    }
+}
